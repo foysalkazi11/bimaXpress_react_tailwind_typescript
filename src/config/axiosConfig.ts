dabc40aae0d72d1306af4422819b719fdd227987
Vikrant Gupta <vikrantgupta.psit@gmail.com>
2021-12-27 18:55:45 +0530

    heroku push

--- src/config/axiosConfig.ts
+++ src/config/axiosConfig.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // const API = "ec2-3-110-207-150.ap-south-1.compute.amazonaws.com";
 const instance = axios.create({
-  baseURL: "/api/",
+  // baseURL: "/api/",
   // headers: { "content-type": "multipart/form-data" },
 });
 
